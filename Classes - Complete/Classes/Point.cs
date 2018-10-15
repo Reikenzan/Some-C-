fee4321e8dea7f102035e3c257f3a9003faa70bb
Debora Urrutia <deboraut@gmail.com>
2018-10-15 19:33:12 -0400

    Add files via upload

---
+++ Classes - Complete/Classes/Point.cs
@@ -0,0 +1,59 @@
+﻿//DEBORA URRUTIA
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        private int x, y;//declare variables
+        private static int objectCount = 0;//set the object counter to 0
+        Random randomizer = new Random();// initialize random for random points
+
+        public Point()// default constructor
+        {
+            this.x = -1;
+            this.y = -1;
+            objectCount++;
+        }
+
+        public Point(int x, int y)// constructor
+        {
+            this.x = x;
+            this.y = y;
+            objectCount++;
+        }
+
+        public Point NewP(int x, int y)//generates a new random number
+        {
+            //-------------------------------------//
+            this.x = randomizer.Next(1,2000);
+            this.y = randomizer.Next(1,2000);
+
+            Point otherpoint = new Point(x,y);
+            //distance = bottomright.DistanceTo(x,y);
+            objectCount++;
+            return otherpoint;
+            //--------------------------------------//
+        }
+
+        //Function used to calculate de distance between points.
+        public double DistanceTo(Point other) 
+        {
+            int xDiff = this.x - other.x;
+            int yDiff = this.y - other.y;
+            double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+            return distance;
+        }
+
+        public static int ObjectCount()//Function that returns the counted objects
+        {            
+            return objectCount;
+        }
+    }
+}
---
+++ Classes - Complete/Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿//DEBORA URRUTIA
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();// default constructor
+            Point bottomRight = new Point(1366, 768); // a setted point
+            Point NewP = new Point();// new random point
+            double distance = origin.DistanceTo(bottomRight);//Distance from point 1(origin) to point 2(bottomRight)
+            Console.WriteLine("Distance is: {0}", distance); //Print the distance for the 2 setted points
+            //Print the number of point objects
+            Console.WriteLine("Number of Point objects: {0}", Point.ObjectCount()); 
+            //-----------------//
+            double distance2 = bottomRight.DistanceTo(NewP);//print the distance from Point2(bottomRight) to Point 3(NewP)
+            Console.WriteLine("Distance is: {0}", distance2); //Print the distance
+            //-----------------//
+        }
+
+        static void Main(string[] args)//Main function that loads(run) the all functions
+        {
+            try //throw a exception
+            {
+                doWork();
+            }
+            catch (Exception ex) //catch the exception
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return;
+        }
+    }
+}
