53d9efc6c1093afe313bd46002117c0683531c28
Debora Urrutia <deboraut@gmail.com>
2018-10-15 19:40:59 -0400

    Add files via upload

---
+++ FinalGame/FinalGame/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FinalGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lb_Hits = new System.Windows.Forms.Label();
+            this.lb_Misses = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lb_Hits
+            // 
+            this.lb_Hits.AutoSize = true;
+            this.lb_Hits.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Hits.Location = new System.Drawing.Point(3, 3);
+            this.lb_Hits.Name = "lb_Hits";
+            this.lb_Hits.Size = new System.Drawing.Size(61, 20);
+            this.lb_Hits.TabIndex = 0;
+            this.lb_Hits.Text = "Hits: 0";
+            // 
+            // lb_Misses
+            // 
+            this.lb_Misses.AutoSize = true;
+            this.lb_Misses.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Misses.Location = new System.Drawing.Point(216, 3);
+            this.lb_Misses.Name = "lb_Misses";
+            this.lb_Misses.Size = new System.Drawing.Size(84, 20);
+            this.lb_Misses.TabIndex = 0;
+            this.lb_Misses.Text = "Misses: 0";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(459, 3);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(114, 20);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Time Left: 30";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(221, 472);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(151, 29);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 513);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.lb_Misses);
+            this.Controls.Add(this.lb_Hits);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Hits;
+        private System.Windows.Forms.Label lb_Misses;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ FinalGame/FinalGame/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalGame
+{
+    public partial class Form1 : Form
+    {
+        Random randomizer = new Random();
+
+        int timeLeft;
+        int cordX;
+        int cordY;
+
+        private Rectangle Target = new Rectangle(10, 125, 40, 40);
+
+        public void StartTheGame()
+        {
+            cordX = randomizer.Next(1, 600);
+            cordY = randomizer.Next(1, 610);
+
+            timeLeft = 30;
+            timeLabel.Text = "30 Seconds";
+            timer1.Start();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MaximumSize = new Size(650, 650);
+            this.MinimumSize = new Size(650, 650);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            /*if ()
+            {
+                
+                MessageBox.Show("You win!", "Congrats!");
+                startButton.Enabled = true;
+            }
+            else */if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " Seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry! Try again!");
+            }
+
+            this.Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int ex1 = Target.Location.X;
+            int ey1 = Target.Location.Y;
+            if (timeLeft > 0)
+            {
+                Target.Location = new Point(ex1 += 10, ey1 += 100);
+            }
+            Target.Location = new Point(ex1 -= cordX, ey1 += cordY);
+
+            this.Refresh();
+
+        }
+
+        public void HitDetect()
+        {
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheGame();
+            startButton.Visible =false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics formGraphics;
+
+            System.Drawing.SolidBrush mybrushtarget = new System.Drawing.SolidBrush(System.Drawing.Color.Gold);
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(mybrushtarget, Target);
+            mybrushtarget.Dispose();
+
+        }
+        
+    }
+}
